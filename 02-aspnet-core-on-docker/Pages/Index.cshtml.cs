83f4c09b9e9e22073b56074aa80b991fa1e7228c
SheGe <szymon.sito@hotmail.com>
2019-10-04 21:45:54 +0200

    Create example ASP.NET project (.NET Core 2.2)

---
+++ 02-aspnet-core-on-docker/Pages/Index.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyDemoApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public async void OnGetAsync()
+        {
+            Directory.CreateDirectory("./data");
+            using (var stream = System.IO.File.AppendText("./data/test.log"))
+            {
+                await stream.WriteLineAsync($"{DateTime.Now}: My super message");
+            }
+        }
+    }
+}
